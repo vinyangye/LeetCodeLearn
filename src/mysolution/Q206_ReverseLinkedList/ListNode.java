aa5b1691f929ea161a7967be35f8e844a17f7a9f
vinyangye <vincentyangye@gmail.com>
2022-10-31 21:44:59 +1100

    Sliding window, linkedList

---
+++ src/mysolution/Q206_ReverseLinkedList/ListNode.java
@@ -0,0 +1,25 @@
+package mysolution.Q206_ReverseLinkedList;
+
+/**
+ * @author yeyang
+ * @Date 29/10/22
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/mysolution/Q3_LongestSubstring/Solution.java
@@ -0,0 +1,34 @@
+package mysolution.Q3_LongestSubstring;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author yeyang
+ * @Date 31/10/22
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> window = new HashMap<>();
+        char[] string = s.toCharArray();
+        int size = string.length;
+        int res = 0;
+
+        int left = 0, right = 0;
+
+        while (right < size) {
+            Character a = string[right];
+            right++;
+            window.put(a, window.getOrDefault(a, 0) + 1);
+
+            while (window.get(a) > 1) {
+                Character b = string[left];
+                left++;
+                window.put(b, window.get(b) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
